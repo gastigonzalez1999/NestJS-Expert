ffaf1e51ee5699e41800111557791cd5b2678ac5
Gaston Gonzalez <gastongonzalez2550.gg@gmail.com>
2023-07-02 19:47:39 -0300

    config and endpoints for uploading files

---
+++ teslo-shop/src/files/files.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, Res } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/file-filter';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/file-namer';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('files')
+export class FilesController {
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    return res.sendFile(path);
+  }
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file', { 
+    fileFilter, 
+    //limits: { fileSize: 1000 } 
+    storage: diskStorage({
+      destination: './static/uploads',
+      filename: fileNamer,
+    }),
+  }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.filesService.UploadedFile(file);
+  }
+}
---
+++ teslo-shop/src/files/files.service.ts
@@ -0,0 +1,28 @@
+import { BadRequestException, Injectable, UploadedFile } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { existsSync } from 'fs';
+import { join } from 'path';
+
+@Injectable()
+export class FilesService {
+  constructor(
+    private readonly configService: ConfigService,
+  ) {}
+  UploadedFile(file: Express.Multer.File) {
+    if (!file)
+      throw new BadRequestException('Make sure your sending a file');
+
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
+
+    return { secureUrl };
+  }
+
+  getStaticProductImage (imageName: string) {
+    const path = join(__dirname, '../../static/uploads', imageName);
+
+    if (!existsSync(path))
+      throw new BadRequestException(`No product found with image $imageName}`);
+
+    return path;
+  }
+}
