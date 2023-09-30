6916accd961fb87e99eaafb30389a87db9250694
Gaston Gonzalez <gastongonzalez2550.gg@gmail.com>
2023-09-30 19:52:21 -0300

    Modify get user custom decorator

--- teslo-shop/src/auth/decorators/get-user.decorator.ts
+++ teslo-shop/src/auth/decorators/get-user.decorator.ts
@@ -1,13 +1,13 @@
 import { ExecutionContext, InternalServerErrorException, createParamDecorator } from "@nestjs/common";
 
 export const GetUSer = createParamDecorator(
-  (data, ctx: ExecutionContext) => {
+  (data: string, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
     const user = req.user;
 
     if (!user)
       throw new InternalServerErrorException('User not found');
 
-    return user;
+    return (!data) ? user : user[data];
   }
 );
