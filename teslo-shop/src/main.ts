6f8d8c01cca351f7c4ca0239c4dc025c8e7cff75
Gaston Gonzalez <gastongonzalez2550.gg@gmail.com>
2023-10-18 20:29:57 -0300

    Add swagger

--- teslo-shop/src/main.ts
+++ teslo-shop/src/main.ts
@@ -1,17 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo API')
+    .setDescription('Teslo Shop')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
