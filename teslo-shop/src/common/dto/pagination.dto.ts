a2b2f4f4811892e91568c204c2cc86e85532c1cf
Gaston Gonzalez <gastongonzalez2550.gg@gmail.com>
2023-10-19 22:14:37 -0300

    Add swagger to pagination

--- teslo-shop/src/common/dto/pagination.dto.ts
+++ teslo-shop/src/common/dto/pagination.dto.ts
@@ -1,12 +1,21 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsOptional, IsPositive, Min } from "class-validator";
 
 export class PaginationDto {
+  @ApiPropertyOptional({
+    default: 10,
+    description: 'Rows needed',
+  })
   @IsOptional()
   @IsPositive()
-  @Type(() => Number) // the same as gobalPipe enableImplicitConversions: true
+  @Type(() => Number) // the same as globalPipe enableImplicitConversions: true
   limit?: number;
 
+  @ApiPropertyOptional({
+    default: 0,
+    description: 'Rows skipped',
+  })
   @IsOptional()
   @Type(() => Number)
   @Min(0)
