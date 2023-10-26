f0c57c62c837c82339e5ace139aad479fe99f089
Gaston Gonzalez <gastongonzalez2550.gg@gmail.com>
2023-10-26 20:30:33 -0300

    Update login user dto with swagger

--- teslo-shop/src/auth/dto/login-user.dto.ts
+++ teslo-shop/src/auth/dto/login-user.dto.ts
@@ -1,10 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class LoginUserDto {
+  @ApiProperty({
+    description: 'User email',
+    nullable: false,
+  })
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'User password',
+    nullable: false,
+    minLength: 6,
+    maxLength: 50,
+  })
   @IsString()
   @MinLength(6)
   @MaxLength(50)
