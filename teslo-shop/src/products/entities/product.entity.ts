e24ade9477be40f6354656e0008795f343e5a0ae
Gaston Gonzalez <gastongonzalez2550.gg@gmail.com>
2023-10-27 17:39:36 -0300

    Update product entity with swagger and add user

--- teslo-shop/src/products/entities/product.entity.ts
+++ teslo-shop/src/products/entities/product.entity.ts
@@ -1,45 +1,88 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+
 import { ProductImage } from './product-image.entity';
+import { User } from 'src/auth/entities/user.entity';
 
 @Entity({ name: 'products' })
 export class Product {
+  @ApiProperty({
+    example: '550e8400-e29b-41d4-a716-446655440000',
+    description: 'Product Id',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 'Teslo T-Shirt',
+    description: 'Product title',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   title: string;
 
+  @ApiProperty({
+    example: 100,
+    description: 'Product price',
+  })
   @Column('float', {
     default: 0,
   })
   price: number;
 
+  @ApiProperty({
+    example: 'Stamped T-Shirt',
+    description: 'Product description',
+  })
   @Column({
     type: 'text',
     nullable: true,
   })
   description: string;
 
+  @ApiProperty({
+    example: 'teslo_t_shirt',
+    description: 'Product Slug for SEO',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   slug: string;
 
+  @ApiProperty({
+    example: 10,
+    description: 'Product stock',
+  })
   @Column('int', {
     default: 0,
   })
   stock: number;
 
+  @ApiProperty({
+    example: ['M', 'S', 'L'],
+    description: 'Product Sizes',
+  })
   @Column('text', {
     array: true,
   })
   sizes: string[];
 
+  @ApiProperty({
+    example: 'Men',
+    description: 'Product gender',
+  })
   @Column('text')
   gender: string;
 
+  @ApiProperty({
+    example: 'shirt',
+    isArray: true,
+    default: [],
+  })
   @Column('text', {
     array: true,
     default: [],
@@ -47,12 +90,23 @@ export class Product {
   tags: string[];
 
   //Relations
+  @ApiProperty({
+    example: ['t-shirt.jpg'],
+    description: 'Product images',
+  })
   @OneToMany(
     () => ProductImage,
     productImage => productImage.product,
     { cascade: true, eager: true },
   )
   images?: ProductImage[];
+
+  @ManyToOne(
+    () => User,
+    (user) => user.product,
+    { eager: true },
+  )
+  user: User;
 
   @BeforeInsert()
   checkSlugInsert() {
