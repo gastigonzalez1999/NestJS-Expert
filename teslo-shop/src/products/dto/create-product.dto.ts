482708ff394ad3769c9abcd4a2b91a2e00916f25
Gaston Gonzalez <gastongonzalez2550.gg@gmail.com>
2023-10-28 22:17:52 -0300

    Update product dto w/ swagger

--- teslo-shop/src/products/dto/create-product.dto.ts
+++ teslo-shop/src/products/dto/create-product.dto.ts
@@ -1,40 +1,83 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product title',
+    nullable: false,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   title: string
 
+  @ApiPropertyOptional({
+    description: 'Product price',
+    nullable: false,
+    minimum: 0,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiPropertyOptional({
+    description: 'Product description',
+    nullable: false,
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiPropertyOptional({
+    description: 'Product slug',
+    nullable: false,
+  })
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiPropertyOptional({
+    description: 'Product stock',
+    nullable: false,
+    minimum: 0,
+  })
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty({
+    description: 'Product sizes',
+    nullable: false,
+    isArray: true,
+  })
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty({
+    description: 'Product gender',
+    nullable: false,
+  })
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiPropertyOptional({
+    description: 'Product tags',
+    nullable: false,
+    isArray: true,
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiPropertyOptional({
+    description: 'Product images',
+    nullable: false,
+    isArray: true,
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
