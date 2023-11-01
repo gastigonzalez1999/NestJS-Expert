84c9318e419ee13506367495bdbf5d410d8ae427
Gaston Gonzalez <gastongonzalez2550.gg@gmail.com>
2023-10-31 21:39:00 -0300

    Update create user with swagger

--- teslo-shop/src/auth/dto/create-user.dto.ts
+++ teslo-shop/src/auth/dto/create-user.dto.ts
@@ -1,10 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User email',
+    nullable: false,
+  })
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'User password',
+    nullable: false,
+    minLength: 6,
+    maxLength: 50,
+  })
   @IsString()
   @MinLength(6)
   @MaxLength(50)
@@ -14,6 +25,11 @@ export class CreateUserDto {
   })
   password: string;
 
+  @ApiProperty({
+    description: 'User full name',
+    nullable: false,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   fullName: string
