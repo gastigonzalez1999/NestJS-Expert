795c902d9919b36c35ae3d3a6e43a704fc987096
Gaston Gonzalez <gastongonzalez2550.gg@gmail.com>
2023-11-01 22:10:56 -0300

    Update auth controller with swagger and new methods

--- teslo-shop/src/auth/auth.controller.ts
+++ teslo-shop/src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Post, Body, UseGuards, Req, SetMetadata } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
-import { AuthGuard } from '@nestjs/passport';
-import { GetUSer } from './decorators/get-user.decorator';
+import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
 import { RawHeaders } from './decorators/raw-headers.decorator';
 import { UserRoleGuard } from 'src/guards/user-role.guard';
@@ -11,15 +13,20 @@ import { RoleProtected } from './decorators/role-protected.decorator';
 import { ValidRoles } from './interfaces/valid-roles';
 import { Auth } from './decorators/auth.decorator';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiResponse({ status: 201, 'description': 'User was created', type: User })
+  @ApiResponse({ status: 400, 'description': 'Bad request' })
   @Post('register')
   create(@Body() body: CreateUserDto) {
     return this.authService.create(body);
   }
 
+  @ApiResponse({ status: 200, 'description': 'User was logged in', type: User })
+  @ApiResponse({ status: 400, 'description': 'Bad request' })
   @Post('login')
   login(@Body() body: LoginUserDto) {
     return this.authService.login(body);
@@ -29,8 +36,8 @@ export class AuthController {
   @UseGuards(AuthGuard())
   testingPrivateRoute(
     @Req() request: Express.Request,
-    @GetUSer() user: User,
-    @GetUSer('email') userEmail: string,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
     @RawHeaders() rawHeaders: string[],
   ) {
     return {
@@ -46,7 +53,7 @@ export class AuthController {
   //@SetMetadata('roles',  ['admin', 'super-user'])
   @UseGuards(AuthGuard(), UserRoleGuard)
   privateRoute2(
-    @GetUSer() user: User,
+    @GetUser() user: User,
   ) {
     return {
       ok: true,
@@ -57,11 +64,22 @@ export class AuthController {
   @Get('private3')
   @Auth()
   privateRout32(
-    @GetUSer() user: User,
+    @GetUser() user: User,
   ) {
     return {
       ok: true,
       user,
     }
+  }
+
+  @ApiResponse({ status: 200, type: User })
+  @ApiResponse({ status: 400, 'description': 'Bad request' })
+  @ApiResponse({ status: 403, 'description': 'Forbidden - Token' })
+  @Get('check-status')
+  @Auth()
+  checkAuthStatus(
+    @GetUser() user: User,
+  ) {
+    return this.authService.checkAuthStatus(user);
   }
 }
