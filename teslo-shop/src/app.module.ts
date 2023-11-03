c8b50c393c51f2bae6bce1f331b565cc606b8ab3
Gaston Gonzalez <gastongonzalez2550.gg@gmail.com>
2023-11-02 22:52:21 -0300

    Update app module with prod config and swagger

--- teslo-shop/src/app.module.ts
+++ teslo-shop/src/app.module.ts
@@ -8,11 +8,18 @@ import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod' ? true: false,
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+              ? { rejectUnauthorized: false }
+              : null,
+      },
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
       port: +process.env.POSTGRES_PORT,
@@ -31,6 +38,7 @@ import { AuthModule } from './auth/auth.module';
     SeedModule,
     FilesModule,
     AuthModule,
+    MessagesWsModule,
   ],
   controllers: [],
   providers: [],
